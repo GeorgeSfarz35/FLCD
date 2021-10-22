c9144dea5b660c1e2bb2edb9bcf587257f2c69bd
GeorgeSfarz35 <91667058+GeorgeSfarz35@users.noreply.github.com>
2021-10-22 12:59:04 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,22 +1,42 @@
 class BSTNode:
-    def __init__(self, identifier=None,  value=None):
+    def __init__(self, identifier=None):
         self.left = None
         self.right = None
         self.ident = identifier
-        self.val = value
 
-    def insert(self, ident, val):
+    def insert(self, ident):
         if self.ident == ident:
             return -1
 
         if ident < self.ident:
             if self.left:
-                self.left.insert(ident, val)
+                self.left.insert(ident)
                 return
-            self.left = BSTNode(ident, val)
+            self.left = BSTNode(ident)
             return
 
         if self.right:
-            self.right.insert(ident, val)
+            self.right.insert(ident)
             return
-        self.right = BSTNode(ident, val)
+        self.right = BSTNode(ident)
+
+    def get(self, ident):
+        if self.ident == ident:
+            return self
+
+        if ident < self.ident:
+            if self.left:
+                return self.left.get(ident)               
+            return "Identifier does not exist"
+
+        if self.right:
+            return self.right.get(ident)
+        return "Identifier does not exist"
+
+
+if __name__ == "__main__":
+    identifiers = BSTNode()
+    constants = BSTNode()
+    identifiers.ident = "name1"
+    identifiers.insert("name2")
+    print(identifiers.get("name2").ident)
